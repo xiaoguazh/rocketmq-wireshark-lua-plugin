d4300373cc8d87eb2bf9c505e54d120c4aac317b
Gary <zhaoxiaoguang@dataguard.com>
2021-08-20 14:57:38 +0800

    Update code and readme

--- README.md
+++ README.md
@@ -1,19 +1,66 @@
-alibaba rocketmq protocol wireshark lua plugin
+# 1 Brief
 
-usage:
-1. find the plugin dir,  On my Mac, the path is "/Applications/WireShark.app/Contents/Resources/share/wireshark"
+This is improved version base on [arthur-zhang](https://github.com/arthur-zhang)/**[rocketmq-wireshark-lua-plugin](https://github.com/arthur-zhang/rocketmq-wireshark-lua-plugin)**
 
-2. mkdir rocketmq-plugin
+Major improvements:
 
-3. copy json.lua and rocketmq.lua to plugin dir "rocketmq-plugin".
+- Support TCP reassembly
 
-4. modify last line of  init.lua
-dofile("rocketmq-plugin/rocketmq.lua")
+- Support parsing when pull message body has multi-records
 
-5. restart wireshark to make it done
+- Replace json.lua with rxi's json.lua
 
-ps, you may also need to modify the port your rocketmq is using, open rocketmq.lua, find the following line. Add your ports here:
+- Add "msg_encoder" variable for handling Chinese chars, default setting is ENC_UTF_8
+
+- Add Tree Fields : total_length, header_length, header, body to ease the inspection
+
+- Add screenshots
+
+- Fix the parsing errors  renaming code: var and methods
+
+# 2 Usage
 
 ```
-local PORTS = { 9876, 10911, 20111 }
+1)Exact and copy "rocketmq-plugin" folder to wireshark installtion folder
+
+    e.g.,
+        C:\Program Files\Wireshark\rocketmq-plugin
+
+2)Open any text editor app and edit init.lua
+    Note: for windows, the text editor requires to be "Run as Adminitrator"
+
+    The init.lua is located in wireshark installtion folder, e.g.,
+        C:\Program Files\Wireshark\init.lua
+
+    Add below line to the end of init.lua, and save
+
+        dofile(DATA_DIR.."rocketmq-plugin" .. package.config:sub(1,1) .. "rocketmq.lua")
+
+3)Open wireshark and load a rocket_mq packet
 ```
+
+# 3 Customization
+
+## 3.1 Customize MQ_PORTS
+
+In case you changed the default Rocket MQ ports to something different, please update **MQ_PORTS **in rocketmq.lua.
+
+```
+local MQ_PORTS = { 9876, 10911, 20111 }
+```
+
+## 3.2 Customize Encoding
+
+In case your need other encoding than UTF-8, please update **msg_encoder** in rocketmq.lua.
+
+See init.lua for more supported encoding list, e.g.,ENC_GB18030 = 80 for Chinese.
+
+```
+local msg_encoder = ENC_UTF_8
+```
+
+# 4 Version History
+
+| Version | Date        | Author   | Note           |
+|:-------:| ----------- | -------- | -------------- |
+| 0.1     | 2021-Aug-20 | xiaoguaz | First delivery |
---
+++ rocketmq-plugin/rocketmq.lua
@@ -0,0 +1,413 @@
+package.prepend_path("rocketmq-plugin")
+local json = dofile(DATA_DIR.."rocketmq-plugin" .. package.config:sub(1,1) .. "json.lua")
+
+-- customizable based on MQ Port setting
+local MQ_PORTS = { 9876, 10911, 20111 }
+
+local NAME = "RocketMQ"
+local proto_MQ = Proto.new(NAME, "RocketMQ Protocol")
+
+-- customizable based on message encoding send from your app
+local msg_encoder = ENC_UTF_8
+    -- see more Encodings from wireshark init.lua, e.g.,ENC_GB18030 = 80 for chinese
+
+-- define tree fields
+local tree_fields = proto_MQ.fields
+
+tree_fields.total_length  = ProtoField.new("Total Length", "total_length",ftypes.INT32)
+tree_fields.header_length = ProtoField.new("Header Length", "header_length",ftypes.INT32)
+tree_fields.header        = ProtoField.new("Header data", "header",ftypes.STRING)
+tree_fields.body          = ProtoField.new("Body data", "body",ftypes.STRING)
+
+local MQ_REQUEST_CODE = {
+    [10] = "SEND_MESSAGE",
+    [11] = "PULL_MESSAGE",
+    [12] = "QUERY_MESSAGE",
+    [13] = "QUERY_BROKER_OFFSET",
+    [14] = "QUERY_CONSUMER_OFFSET",
+    [15] = "UPDATE_CONSUMER_OFFSET",
+    [17] = "UPDATE_AND_CREATE_TOPIC",
+    [21] = "GET_ALL_TOPIC_CONFIG",
+    [22] = "GET_TOPIC_CONFIG_LIST",
+    [23] = "GET_TOPIC_NAME_LIST",
+    [25] = "UPDATE_BROKER_CONFIG",
+    [26] = "GET_BROKER_CONFIG",
+    [27] = "TRIGGER_DELETE_FILES",
+    [28] = "GET_BROKER_RUNTIME_INFO",
+    [29] = "SEARCH_OFFSET_BY_TIMESTAMP",
+    [30] = "GET_MAX_OFFSET",
+    [31] = "GET_MIN_OFFSET",
+    [32] = "GET_EARLIEST_MSG_STORETIME",
+    [33] = "VIEW_MESSAGE_BY_ID",
+    [34] = "HEART_BEAT",
+    [35] = "UNREGISTER_CLIENT",
+    [36] = "CONSUMER_SEND_MSG_BACK",
+    [37] = "END_TRANSACTION",
+    [38] = "GET_CONSUMER_LIST_BY_GROUP",
+    [39] = "CHECK_TRANSACTION_STATE",
+    [40] = "NOTIFY_CONSUMER_IDS_CHANGED",
+    [41] = "LOCK_BATCH_MQ",
+    [42] = "UNLOCK_BATCH_MQ",
+    [43] = "GET_ALL_CONSUMER_OFFSET",
+    [45] = "GET_ALL_DELAY_OFFSET",
+    [100] = "PUT_KV_CONFIG",
+    [101] = "GET_KV_CONFIG",
+    [102] = "DELETE_KV_CONFIG",
+    [103] = "REGISTER_BROKER",
+    [104] = "UNREGISTER_BROKER",
+    [105] = "GET_ROUTEINTO_BY_TOPIC",
+    [106] = "GET_BROKER_CLUSTER_INFO",
+    [200] = "UPDATE_AND_CREATE_SUBSCRIPTIONGROUP",
+    [201] = "GET_ALL_SUBSCRIPTIONGROUP_CONFIG",
+    [202] = "GET_TOPIC_STATS_INFO",
+    [203] = "GET_CONSUMER_CONNECTION_LIST",
+    [204] = "GET_PRODUCER_CONNECTION_LIST",
+    [205] = "WIPE_WRITE_PERM_OF_BROKER",
+    [206] = "GET_ALL_TOPIC_LIST_FROM_NAMESERVER",
+    [207] = "DELETE_SUBSCRIPTIONGROUP",
+    [208] = "GET_CONSUME_STATS",
+    [209] = "SUSPEND_CONSUMER",
+    [210] = "RESUME_CONSUMER",
+    [211] = "RESET_CONSUMER_OFFSET_IN_CONSUMER",
+    [212] = "RESET_CONSUMER_OFFSET_IN_BROKER",
+    [213] = "ADJUST_CONSUMER_THREAD_POOL",
+    [214] = "WHO_CONSUME_THE_MESSAGE",
+    [215] = "DELETE_TOPIC_IN_BROKER",
+    [216] = "DELETE_TOPIC_IN_NAMESRV",
+    [219] = "GET_KVLIST_BY_NAMESPACE",
+    [220] = "RESET_CONSUMER_CLIENT_OFFSET",
+    [221] = "GET_CONSUMER_STATUS_FROM_CLIENT",
+    [222] = "INVOKE_BROKER_TO_RESET_OFFSET",
+    [223] = "INVOKE_BROKER_TO_GET_CONSUMER_STATUS",
+    [300] = "QUERY_TOPIC_CONSUME_BY_WHO",
+    [224] = "GET_TOPICS_BY_CLUSTER",
+    [301] = "REGISTER_FILTER_SERVER",
+    [302] = "REGISTER_MESSAGE_FILTER_CLASS",
+    [303] = "QUERY_CONSUME_TIME_SPAN",
+    [304] = "GET_SYSTEM_TOPIC_LIST_FROM_NS",
+    [305] = "GET_SYSTEM_TOPIC_LIST_FROM_BROKER",
+    [306] = "CLEAN_EXPIRED_CONSUMEQUEUE",
+    [307] = "GET_CONSUMER_RUNNING_INFO",
+    [308] = "QUERY_CORRECTION_OFFSET",
+    [309] = "CONSUME_MESSAGE_DIRECTLY",
+    [310] = "SEND_MESSAGE_V2",
+    [311] = "GET_UNIT_TOPIC_LIST",
+    [312] = "GET_HAS_UNIT_SUB_TOPIC_LIST",
+    [313] = "GET_HAS_UNIT_SUB_UNUNIT_TOPIC_LIST",
+    [314] = "CLONE_GROUP_OFFSET",
+    [315] = "VIEW_BROKER_STATS_DATA",
+    [316] = "CLEAN_UNUSED_TOPIC",
+    [317] = "GET_BROKER_CONSUME_STATS",
+    [318] = "UPDATE_NAMESRV_CONFIG",
+    [319] = "GET_NAMESRV_CONFIG",
+    [320] = "SEND_BATCH_MESSAGE",
+    [321] = "QUERY_CONSUME_QUEUE",
+    [322] = "QUERY_DATA_VERSION",
+}
+
+local MQ_RESPONSE_CODE = {
+    [0] = "SUCCESS",
+    [1] = "SYSTEM_ERROR",
+    [2] = "SYSTEM_BUSY",
+    [3] = "REQUEST_CODE_NOT_SUPPORTED",
+    [4] = "TRANSACTION_FAILED",
+    [10] = "FLUSH_DISK_TIMEOUT",
+    [11] = "SLAVE_NOT_AVAILABLE",
+    [12] = "FLUSH_SLAVE_TIMEOUT",
+    [13] = "MESSAGE_ILLEGAL",
+    [14] = "SERVICE_NOT_AVAILABLE",
+    [15] = "VERSION_NOT_SUPPORTED",
+    [16] = "NO_PERMISSION",
+    [17] = "TOPIC_NOT_EXIST",
+    [18] = "TOPIC_EXIST_ALREADY",
+    [19] = "PULL_NOT_FOUND",
+    [20] = "PULL_RETRY_IMMEDIATELY",
+    [21] = "PULL_OFFSET_MOVED",
+    [22] = "QUERY_NOT_FOUND",
+    [23] = "SUBSCRIPTION_PARSE_FAILED",
+    [24] = "SUBSCRIPTION_NOT_EXIST",
+    [25] = "SUBSCRIPTION_NOT_LATEST",
+    [26] = "SUBSCRIPTION_GROUP_NOT_EXIST",
+    [200] = "TRANSACTION_SHOULD_COMMIT",
+    [201] = "TRANSACTION_SHOULD_ROLLBACK",
+    [202] = "TRANSACTION_STATE_UNKNOW",
+    [203] = "TRANSACTION_STATE_GROUP_WRONG",
+    [204] = "NO_BUYER_ID",
+    [205] = "NOT_IN_CURRENT_UNIT",
+    [206] = "CONSUMER_NOT_ONLINE",
+    [207] = "CONSUME_MSG_TIMEOUT",
+    [208] = "NO_MESSAGE",
+}
+
+-- identify the request or reponse
+function is_up_direction(dstPort)
+    for _, port in ipairs(MQ_PORTS) do
+        if (dstPort == port) then
+            return true
+        end
+    end
+    return false
+end
+
+-- decode the body data (remark=FOUND), support multi-rows data
+function decode_body_ext(bodyTree, pinfo, bodyData, bodyLen)
+    pinfo.cols.info:append(">>>>#FOUND#")
+    
+    local index = 0;
+    
+    local offset = 0;
+    
+    while offset < bodyLen do
+      
+      local subBodyTree = bodyTree:add("data [ ".. tostring(index) .. " ]", "")
+      
+      subBodyTree:add("totalSize", bodyData(offset, 4):int())
+      offset = offset + 4;
+
+      local magicCode = string.format("0X%8.8X", bodyData(offset, 4):uint())
+      subBodyTree:add("magicCode", magicCode)
+      offset = offset + 4;
+
+      subBodyTree:add("bodyCRC", bodyData(offset, 4):int())
+      offset = offset + 4;
+
+      subBodyTree:add("queueId", bodyData(offset, 4):int())
+      offset = offset + 4;
+
+      subBodyTree:add("flag", bodyData(offset, 4):int())
+      offset = offset + 4;
+
+      subBodyTree:add("queueOffset", bodyData(offset, 8):int64():tonumber())
+      offset = offset + 8;
+
+      subBodyTree:add("physicOffset", bodyData(offset, 8):int64():tonumber())
+      offset = offset + 8;
+
+      subBodyTree:add("sysFlag", bodyData(offset, 4):int())
+      offset = offset + 4;
+
+      subBodyTree:add("bornTimeStamp", bodyData(offset, 8):int64():tonumber())
+      offset = offset + 8;
+
+      local bornHost = bodyData(offset, 1):uint()
+              .. "." .. bodyData(offset + 1, 1):uint()
+              .. "." .. bodyData(offset + 2, 1):uint()
+              .. "." .. bodyData(offset + 3, 1):uint()
+
+      subBodyTree:add("bornHost", bornHost)
+      offset = offset + 4;
+
+      subBodyTree:add("port", bodyData(offset, 4):int())
+      offset = offset + 4;
+      subBodyTree:add("storeTimestamp", bodyData(offset, 8):int64():tonumber())
+      offset = offset + 8;
+
+      local storeHost = bodyData(offset, 1):uint()
+              .. "." .. bodyData(offset + 1, 1):uint()
+              .. "." .. bodyData(offset + 2, 1):uint()
+              .. "." .. bodyData(offset + 3, 1):uint()
+      subBodyTree:add("storeHost", storeHost)
+      offset = offset + 4;
+
+      subBodyTree:add("storePort", bodyData(offset, 4):int())
+      offset = offset + 4;
+
+      --13 RECONSUMETIMES
+      subBodyTree:add("reconsumeTimes", bodyData(offset, 4):int())
+      offset = offset + 4;
+      --14 Prepared Transaction Offset
+      subBodyTree:add("preparedTransactionOffset", bodyData(offset, 8):int64():tonumber())
+      offset = offset + 8;
+      --15 BODY
+      local bodyLen = bodyData(offset, 4):int()
+      --            subBodyTree:add("bodyLen", bodyLen)
+      offset = offset + 4;
+
+      subBodyTree:add("body:", bodyData(offset, bodyLen):string())
+      offset = offset + bodyLen;
+
+      --16 TOPIC
+      local topicLen = bodyData(offset, 1):int()
+      offset = offset + 1;
+      --            subBodyTree:add("topicLen", topicLen)
+      local topic = bodyData(offset, topicLen):string()
+      subBodyTree:add("topic:", topic)
+      -- pinfo.cols.info:append(" topic:" .. topic)
+
+      offset = offset + topicLen;
+
+      --17 properties
+      local propertiesLength = bodyData(offset, 2):int()
+      offset = offset + 2;
+      subBodyTree:add("propertiesLength", propertiesLength)
+
+      if (propertiesLength > 0) then
+          local propertiesStr = bodyData(offset, propertiesLength):string()
+          offset = offset + propertiesLength
+          local propertiesTree = subBodyTree:add("propertiesStr", "size: " .. propertiesLength)
+          for k, v in string.gmatch(propertiesStr, "(%w+)\1(%w+)") do
+              propertiesTree:add(k, v)
+          end
+      end
+      
+      index = index + 1
+      
+    end
+
+end
+
+-- parse k-v from table and add to tree
+function parse_kv_and_addto_tree(k, v, tree)
+    if (type(v) == 'table') then
+        local sizeStr = ""
+        if (#v > 0) then
+            sizeStr = "size: " .. #v
+        end;
+        local childTree = tree:add(k, sizeStr, tree)
+        for key, value in pairs(v) do
+            parse_kv_and_addto_tree(key, value, childTree)
+        end
+    else
+        tree:add(k .. ":", json.encode(v))
+    end
+end
+
+-- do dissector of mq packet
+function do_dissector(tvb, pinfo, tree)
+    local srcPort = pinfo.src_port;
+    local dstPort = pinfo.dst_port;
+
+    local subtree = tree:add(proto_MQ, tvb())
+    pinfo.cols.protocol = proto_MQ.name;
+    pinfo.cols.info = ""
+
+    local length = tvb(0, 4):uint()
+    subtree:add(tree_fields.total_length, tvb(0, 4))
+    
+    local headerLength = tvb(4, 4):uint()
+    subtree:add(tree_fields.header_length , tvb(4, 4))
+    subtree:add("Data Length" , length - headerLength - 4)
+    
+    -- handle header data (json)
+    local headerData = tvb(8, headerLength):string(msg_encoder)
+    local headerTree = subtree:add(tree_fields.header, tvb(8, headerLength), "")
+
+    print ("header = ", headerData)
+    
+    local header_json = json.decode(headerData)
+
+    -- add header content to header tree
+    for k, v in pairs(header_json) do
+        parse_kv_and_addto_tree(k, v, headerTree)
+    end
+
+    -- header->remark : FOUND, NOT_FOUND, ...
+    local is_response_remark_found = false
+
+    if (is_up_direction(dstPort)) then
+        --handle request
+        pinfo.cols.info:append("[REQUEST]" .. "↑↑↑")
+
+        -- retrieve header->code
+        for k, v in pairs(header_json) do
+            if (k == "code") then
+                local codeStr = MQ_REQUEST_CODE[v];
+                if (codeStr == nil) then
+                    break;
+                end
+                pinfo.cols.info:append(" code=" .. v .. "(" .. codeStr .. ")")
+            end
+        end
+
+        -- retrieve header->topic
+        for k, v in pairs(header_json) do
+            if (k == "extFields") then
+                if (v["topic"] ~= nil) then
+                    pinfo.cols.info:append(" topic:" .. v["topic"])
+                end
+            end
+        end
+    else
+        -- handle response
+        pinfo.cols.info:append("[RESPONSE]" .. "↓↓↓")
+
+        -- retrieve header->code
+        for k, v in pairs(header_json) do
+            if (k == "code") then
+                local codeStr = MQ_RESPONSE_CODE[v];
+                if (codeStr == nil) then
+                    break;
+                else
+                  pinfo.cols.info:append(" code=" .. v .. "(" .. codeStr .. ")")
+                end
+            end
+        end
+        
+        -- handle header->remark
+        local remark = header_json["remark"]
+
+        if (remark ~= nil and remark == "FOUND") then
+            is_response_remark_found = true
+        end
+    end
+
+    -- handle body
+    -- local bodyDataLen = tvb:len() - 8 - headerLength
+    local bodyDataLen = length - headerLength - 4
+
+    if (bodyDataLen <= 0) then
+        return
+    end
+    
+    local bodyData = tvb(8 + headerLength, bodyDataLen)
+    local bodyTree = subtree:add(tree_fields.body, tvb(8 + headerLength, bodyDataLen),"")
+
+    if (bodyData ~= nil and bodyData:len() > 0) then
+        if (not is_response_remark_found) then
+            bodyDataStr = bodyData:string(msg_encoder)
+            -- print ("remark = False")
+            print ("body = ", bodyDataStr)
+            
+            if (bodyDataStr:sub(1, 1) == '{' or bodyDataStr:sub(1, 1) == '[') then
+                -- in case json data
+                local body = json.decode(bodyDataStr)
+                for k, v in pairs(body) do
+                    parse_kv_and_addto_tree(k, v, bodyTree)
+                end
+            else
+                -- in case non-json
+                bodyTree:add("Body:", bodyDataStr)
+            end
+        else
+            --remark = FOUND, means need decode base on RocketMQ body structure
+            -- print ("remark = True")
+            print ("body = ", bodyData)
+            
+            decode_body_ext(bodyTree, pinfo, bodyData, bodyDataLen)
+        end
+    end
+
+end
+
+-- wireshark api of dissector
+function proto_MQ.dissector(tvb, pinfo, tree)
+    -- for tcp reassembliy
+    local pdu_len = tvb(0, 4):uint() + 4
+    local tvb_len = tvb:len()
+    -- local offset = pinfo.desegment_offset or 0
+
+    if pdu_len > tvb_len then
+      pinfo.desegment_len = pdu_len - tvb_len
+      -- pinfo.desegment_offset = offset
+
+      return
+    else
+      do_dissector(tvb, pinfo, tree)
+    end
+
+end
+
+-- wireshark api of register dissector
+for _, port in ipairs(MQ_PORTS) do
+    DissectorTable.get("tcp.port"):add(port, proto_MQ)
+end
